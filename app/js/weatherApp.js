49235e7df2aa7032c87f7dbe10a23493fb129998
RamaRaju V <RamaRaju V>
2017-01-29 21:41:28 -0500

    - Implemented jsonwebtoken based authentication to restrict permission only to certain users. - Also added a very basic login page using Angular Material

--- app/js/weatherApp.js
+++ app/js/weatherApp.js
@@ -3,13 +3,23 @@
 // Declare app level module which depends on filters, and services
 angular.module('weatherApp', [
   'ngRoute',
+  'ngStorage',
   'weatherApp.filters',
   'iso-3166-country-codes',
   'weatherApp.services',
   'weatherApp.directives',
   'weatherApp.controllers'
 ])
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider.when('/forecast', { templateUrl: 'partials/forecast.html', controller: 'WeatherAppCtrl' });
-  $routeProvider.otherwise({ redirectTo: '/forecast' });
+  $routeProvider.when('/login', { templateUrl: 'partials/login.html', controller: 'LoginCtrl' });
+  $routeProvider.otherwise({ redirectTo: '/login' });
+
+  if (window.history && window.history.pushState) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }
+
 }]);
---
+++ scripts/routes/authRoute.js
@@ -0,0 +1,80 @@
+
+/**
+ * Authentication Router module.
+ * @author Rama Raju Vatsavai <rrv.atwork@gmail.com>
+ * @module routes/authRoute
+ */
+
+var http = require('http');
+var request = require('request');
+var jwt = require("jsonwebtoken");
+var config = require('./authConfig');''
+
+/** The Router class from restify-router module */
+const Router = require('restify-router').Router;
+
+/** An instance of Router class */
+const router = new Router();
+
+/** On a GET request for /auth/users, return the list of users */
+router.get('/auth/users', handleGetUsers);
+
+/** On a POST request for /auth/login, validate the user credentials and generate a token */
+router.post('/auth/login', handleLogin);
+
+/** 
+* Handler function for the GET request
+* @function handleGetUsers
+* @param {Object} req the http request
+* @param {Object} res the http response
+* @param {Object} next the chain handler for routes
+*/
+function handleGetUsers(req, res, next) {
+    res.send(config.users);
+    next();
+}
+
+/**
+ * Handler function for the POST request to handle user validation and Authentication
+ * @function handleLogin
+ * @param {Object} req the http request
+ * @param {Object} res the http response
+ * @param {Object} next the chain handler for routes
+ */
+function handleLogin(req, res, next){
+    if (req.body != null && req.body.username != null){
+
+        var username = req.body.username;
+        var password = req.body.password;
+        var userObj = config.users[username];
+        console.log('userObj ', userObj);
+
+        if (userObj != null && password === userObj.password){
+            var profile = {
+                username: username,
+                password: password
+            }
+
+            // If username and passowrd match, then proceed with token generation and respond
+            var token = jwt.sign(profile, config.secret, {expiresIn: 60*60});
+            res.send({
+                result: true,
+                message: 'Authentication Successful',
+                status: 200,
+                token: token
+            });
+        }
+        else {
+            res.send({
+                result: false,
+                message: 'Authentication Failed',
+                status: 401,
+                token: null
+            })
+        }
+    }
+    next();
+}
+
+/** Export the Router instance */
+module.exports = router;  
